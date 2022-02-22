305449f7d74485c0a7dade903bceffbab842688e
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-02-22 14:38:37 -0300

    refactor(client): change logo text shadow values

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ export default function Header() {
         <div className="container mx-auto px-10 md:px-6 lg:px-0 h-72 flex justify-between items-center">
           <nav>
             <a
-              className="text-shadow-4-6 font-dancingscript text-26 select-none md:text-28 lg:text-32 font-bold text-red-400"
+              className="text-shadow-2-2-6 font-dancingscript text-26 select-none md:text-28 lg:text-32 font-bold text-red-400"
               href="#"
             >
               Aline Balas Gourmet
