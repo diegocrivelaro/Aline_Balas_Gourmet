4b84c5dbaf15b49e6ef96499adba0df8fab9f492
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-01 15:32:26 -0300

    refactor(tailwind.config.js): add social networks div shadow settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,6 +79,7 @@ module.exports = {
       boxShadow: {
         "2-6": "0px 2px 6px rgba(0, 0, 0, 0.2)",
         "8-8": "8px 0px 8px rgba(0, 0, 0, 0.2)",
+        "2-8": "0px 2px 8px rgba(0, 0, 0, 0.2)", // Footer
       }
     },
   },
