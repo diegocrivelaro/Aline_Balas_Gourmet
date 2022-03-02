3c5604636b2b81cb3e3a4106629edaf0abd7c518
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-02 10:32:41 -0300

    refactor(btnwhatsapp): redo button with css
    Added a floating animation on the button

--- components/Btnwhatsapp.js
+++ components/Btnwhatsapp.js
@@ -1,16 +1,17 @@
+import { BsWhatsapp } from 'react-icons/bs';
+
 export default function Btnwhatsapp() {
   return (
     <>
-      <div className="absolute right-4 bottom-0 lg:right-18 xl:right-96">
+      <div className="fixed right-24 bottom-24">
         <a
           href="https://api.whatsapp.com/send?phone=5516993070022&text=Ol%C3%A1%2C%20Aline!"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            src="/static/images/icons/btnwhatsapp.svg"
-            alt="Botão do WhatsApp"
-          />
+          <div className="btn-whatsapp-floating w-56 h-56 sm:w-58 sm:h-58 flex justify-center items-center rounded-full bg-green-500 transition-all duration-300 hover:bg-green-400 hover:scale-105 shadow-btn-whatsapp">
+            <BsWhatsapp color="#fff" className="w-26 h-26 sm:w-28 sm:h-28" />
+          </div>
         </a>
       </div>
     </>
