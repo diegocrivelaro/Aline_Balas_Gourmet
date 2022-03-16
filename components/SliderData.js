eb3885597a8e9926a41318700e7bbc19b68f9a58
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-16 07:44:33 -0300

    chore(components): add slider data component

---
+++ components/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+  {
+    key: 0,
+    src: '/static/logo/logo.webp',
+    alt: 'Slide da logo',
+  },
+  {
+    key: 1,
+    src: '/static/slider/image0.jpg',
+    alt: 'Slide 1',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image1.jpg',
+    alt: 'Slide 2',
+  },
+];
