7eec7e19e87813affa1db94d2d684931a5a394ec
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-29 14:06:30 -0300

    feat(main): add Slider

--- components/Slider.js
+++ components/Slider.js
@@ -1,60 +1,36 @@
-import { useState } from 'react';
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import { Autoplay, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { SliderData } from './SliderData';
 
 export default function Slider() {
-  const [SlideIndex, setSlideIndex] = useState(0);
-  const slideLength = SliderData.length;
-
-  if (!Array.isArray(SliderData) || SliderData.length <= 0) {
-    return null;
-  }
-
-  const prevSlide = () => {
-    setSlideIndex(SlideIndex === 0 ? slideLength - 1 : SlideIndex - 1);
-  };
-
-  const nextSlide = () => {
-    setSlideIndex(SlideIndex === slideLength - 1 ? 0 : SlideIndex + 1);
-  };
-
-  setTimeout(nextSlide, 12000);
-
   return (
     <>
-      <div className="relative flex justify-center items-center w-364 h-346 sm:w-556 sm:h-450">
-        {SliderData.map((image, index) => {
-          return (
-            <div
-              className={
-                index === SlideIndex
-                  ? 'w-full h-full opacity-100 duration-700 ease-in'
-                  : 'opacity-0'
-              }
-              key={index}
-            >
-              {index === SlideIndex && (
+      <div className="w-364 h-346 sm:w-556 sm:h-450 rounded-lg border-2 border-brown-600 sm:rounded-36 sm:border-0 sm:border-none">
+        <Swiper
+          className="w-full h-full"
+          slidePerView={1}
+          spaceBetween={10}
+          navigation={true}
+          autoplay={{
+            delay: 10000,
+            disableOnInteraction: false,
+          }}
+          modules={[Navigation, Autoplay]}
+        >
+          {SliderData.map((image) => {
+            return (
+              <SwiperSlide>
                 <img
-                  className="w-full h-full object-cover rounded-lg border-2 border-brown-600 sm:rounded-36 sm:border-0 sm:border-none"
+                  className="w-full h-full object-cover rounded-lg sm:rounded-36"
                   src={image.src}
                   alt={image.alt}
                 />
-              )}
-            </div>
-          );
-        })}
-        <button
-          className="w-40 h-40 flex justify-center items-center absolute top-1/2 left-6 -translate-y-1/2 select-none bg-gray-200 rounded-full"
-          onClick={prevSlide}
-        >
-          <AiOutlineArrowLeft className="w-30 h-30 sm:w-34 sm:h-34 text-white-default" />
-        </button>
-        <button
-          className="w-40 h-40 flex justify-center items-center absolute top-1/2 right-6 -translate-y-1/2 select-none bg-gray-200 rounded-full"
-          onClick={nextSlide}
-        >
-          <AiOutlineArrowRight className="w-30 h-30 sm:w-34 sm:h-34 text-white-default" />
-        </button>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/btnwhatsapp.css';
 import '../styles/globals.css';
 import '../styles/header.css';
+import '../styles/SwiperSlider.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
