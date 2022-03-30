036959caa879cbd1606dd767cf955aafe96f89bd
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-30 07:46:26 -0300

    chore(public): add new images to the slide

--- components/SliderData.js
+++ components/SliderData.js
@@ -6,12 +6,47 @@ export const SliderData = [
   },
   {
     key: 1,
-    src: '/static/slider/image0.jpg',
+    src: '/static/slider/image01.webp',
     alt: 'Slide 1',
   },
   {
     key: 2,
-    src: '/static/slider/image1.jpg',
+    src: '/static/slider/image02.webp',
+    alt: 'Slide 2',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image03.webp',
+    alt: 'Slide 2',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image04.webp',
+    alt: 'Slide 2',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image05.webp',
+    alt: 'Slide 2',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image06.webp',
+    alt: 'Slide 2',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image07.webp',
+    alt: 'Slide 2',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image08.webp',
+    alt: 'Slide 2',
+  },
+  {
+    key: 2,
+    src: '/static/slider/image09.webp',
     alt: 'Slide 2',
   },
 ];
