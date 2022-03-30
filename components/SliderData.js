ae39ca56ae357e4313c76ac308083913f34c7c0f
Diego Crivelaro <contato.diegocrivelaro@gmail.com>
2022-03-30 07:49:33 -0300

    fix(component): remove line of code from image 8

--- components/SliderData.js
+++ components/SliderData.js
@@ -17,36 +17,31 @@ export const SliderData = [
   {
     key: 2,
     src: '/static/slider/image03.webp',
-    alt: 'Slide 2',
+    alt: 'Slide 3',
   },
   {
     key: 2,
     src: '/static/slider/image04.webp',
-    alt: 'Slide 2',
+    alt: 'Slide 4',
   },
   {
     key: 2,
     src: '/static/slider/image05.webp',
-    alt: 'Slide 2',
+    alt: 'Slide 5',
   },
   {
     key: 2,
     src: '/static/slider/image06.webp',
-    alt: 'Slide 2',
+    alt: 'Slide 6',
   },
   {
     key: 2,
     src: '/static/slider/image07.webp',
-    alt: 'Slide 2',
-  },
-  {
-    key: 2,
-    src: '/static/slider/image08.webp',
-    alt: 'Slide 2',
+    alt: 'Slide 7',
   },
   {
     key: 2,
     src: '/static/slider/image09.webp',
-    alt: 'Slide 2',
+    alt: 'Slide 9',
   },
 ];
